bb214273d4152f57c90e940b0a8a0214b501bb3b
wirelessmusic <alex.hepting@gmail.com>
2023-09-28 06:09:55 +0200

    Update main.py
        Added the validate_description and validate_category methods to ensure that a valid description and category are entered.
    
        Modified the refresh_list method to display the URLs with their descriptions and categories.
    
        Updated the export_csv, export_json, and export_xml methods to prompt the user for a file name and pass it to the respective export functions in the UrlManager class.
    
        Completed the show_insights method to generate a bar chart showing the distribution of URLs by category using matplotlib.
    
        Added the run method to start the application's main loop.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# urls_manager.py
+
 import tkinter as tk
 from tkinter import messagebox, filedialog
 import csv
@@ -7,7 +9,15 @@ import sqlite3
 import matplotlib.pyplot as plt
 
 class UrlManager:
+    """
+    A class to manage URLs in a SQLite database.
+    """
     def __init__(self, db_name='urls.db'):
+        """
+        Initialize the UrlManager.
+
+        :param db_name: Name of the SQLite database.
+        """
         self.conn = sqlite3.connect(db_name)
         self.c = self.conn.cursor()
         self.c.execute('''CREATE TABLE IF NOT EXISTS urls
@@ -18,17 +28,39 @@ class UrlManager:
         self.conn.commit()
 
     def add_url(self, url, description, category):
+        """
+        Add a URL to the database.
+
+        :param url: The URL.
+        :param description: The description of the URL.
+        :param category: The category of the URL.
+        """
         self.c.execute("INSERT INTO urls (url, description, category) VALUES (?, ?, ?)", (url, description, category))
         self.conn.commit()
 
     def delete_url(self, id):
+        """
+        Delete a URL from the database.
+
+        :param id: The ID of the URL to be deleted.
+        """
         self.c.execute("DELETE FROM urls WHERE id=?", (id,))
         self.conn.commit()
 
     def get_urls(self):
+        """
+        Get all URLs from the database.
+
+        :return: A cursor with the URLs.
+        """
         return self.c.execute("SELECT * FROM urls")
 
     def get_insights_data(self):
+        """
+        Get insights data about the distribution of URLs by category.
+
+        :return: A dictionary with categories as keys and counts as values.
+        """
         categories = {}
         for row in self.get_urls():
             category = row[3]
@@ -39,6 +71,11 @@ class UrlManager:
         return categories
 
     def export_csv(self, file_name):
+        """
+        Export URLs to a CSV file.
+
+        :param file_name: The name of the CSV file.
+        """
         with open(file_name, mode='w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(["Description", "URL", "Category"])
@@ -46,6 +83,11 @@ class UrlManager:
                 writer.writerow([row[2], row[1], row[3]])
 
     def export_json(self, file_name):
+        """
+        Export URLs to a JSON file.
+
+        :param file_name: The name of the JSON file.
+        """
         urls = []
         for row in self.get_urls():
             urls.append({"description": row[2], "url": row[1], "category": row[3]})
@@ -53,6 +95,11 @@ class UrlManager:
             json.dump(urls, file, indent=4)
 
     def export_xml(self, file_name):
+        """
+        Export URLs to an XML file.
+
+        :param file_name: The name of the XML file.
+        """
         urls = ET.Element('urls')
         for row in self.get_urls():
             url = ET.SubElement(urls, 'url')
@@ -81,7 +128,7 @@ class UrlManagerApp:
         self.description_entry = tk.Entry(self.app)
         self.description_entry.grid(row=1, column=1, padx=5, pady=5, sticky='we', columnspan=2)
 
-        self.category_label = tk.Label(self.app, text="Category:")  # Added category label
+        self.category_label = tk.Label(self.app, text="Category:")
         self.category_label.grid(row=2, column=0, sticky='e')
         self.category_entry = tk.Entry(self.app)
         self.category_entry.grid(row=2, column=1, padx=5, pady=5, sticky='we', columnspan=2)
@@ -106,7 +153,7 @@ class UrlManagerApp:
         self.export_xml_button = tk.Button(self.app, text="Export to XML", command=self.export_xml)
         self.export_xml_button.grid(row=4, column=2, padx=5, pady=5, sticky='we')
 
-        self.show_insights_button = tk.Button(self.app, text="Show Insights", command=self.show_insights)  # Added Show Insights button
+        self.show_insights_button = tk.Button(self.app, text="Show Insights", command=self.show_insights)
         self.show_insights_button.grid(row=4, column=3, padx=5, pady=5, sticky='we')
 
         self.refresh_list()
@@ -128,11 +175,43 @@ class UrlManagerApp:
             self.url_manager.delete_url(selected_id[0]+1)
             self.refresh_list()
 
+    def validate_url(self, url):
+        if not url:
+            messagebox.showerror("Error", "Please enter a URL.")
+            return False
+        return True
+
+    def validate_description(self, description):
+        if not description:
+            messagebox.showerror("Error", "Please enter a description.")
+            return False
+        return True
+
     def validate_category(self, category):
         if not category:
             messagebox.showerror("Error", "Please enter a category.")
             return False
         return True
+
+    def refresh_list(self):
+        self.url_listbox.delete(0, tk.END)
+        for row in self.url_manager.get_urls():
+            self.url_listbox.insert(tk.END, f"Description: {row[2]}, URL: {row[1]}, Category: {row[3]}")
+
+    def export_csv(self):
+        file_name = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV Files", "*.csv")])
+        if file_name:
+            self.url_manager.export_csv(file_name)
+
+    def export_json(self):
+        file_name = filedialog.asksaveasfilename(defaultextension=".json", filetypes=[("JSON Files", "*.json")])
+        if file_name:
+            self.url_manager.export_json(file_name)
+
+    def export_xml(self):
+        file_name = filedialog.asksaveasfilename(defaultextension=".xml", filetypes=[("XML Files", "*.xml")])
+        if file_name:
+            self.url_manager.export_xml(file_name)
 
     def show_insights(self):
         insights_data = self.url_manager.get_insights_data()
